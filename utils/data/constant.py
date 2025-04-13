04d0f7771716696aa0bc7028784bf2a8ea43257a
Zhiwei Li <zhw.li@outlook.com>
2025-04-13 18:33:03 +0800

    add constant template file

---
+++ utils/data/constant.py
@@ -0,0 +1,8 @@
+# You need to set the following variables in your environment variables
+
+EMAIL_HOST = "email_host"
+SENDER_ADDRESS = "sender_address"
+PASSWORD = "password"
+SENDER_NAME = "sender_name"
+RECEIVER = "receiver"
+AUTODL_TOKEN = "autodl_token"
